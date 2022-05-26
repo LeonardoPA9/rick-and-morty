3a4a9f14ab11c2c2c48c469c19df082a0fa3c082
Leonardo Pesina <leonardo.pesina@nike.com>
2022-05-26 12:03:46 -0500

    commit before adding pagination

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
+import CharacterProvider from "./context/CharacterContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <CharacterProvider>
+      <App />
+    </CharacterProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
